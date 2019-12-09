1fa7c3154f229780bc17b71eeaa74b841dd6a9b0
Gerardo Keys <jotojry@icloud.com>
2019-12-08 21:50:00 -0800

    minor changes to api, to account for mistyped credentials.

--- api/routes/middleware/authentication.js
+++ api/routes/middleware/authentication.js
@@ -5,11 +5,15 @@ const auth = require("basic-auth");
 const authenticateUser = async (req, res, next) => {
     let message = null;
     // Parse the user's credentials from the Authorization header.
+    
     const credentials = auth(req);
+    
     // If the user's credentials are available...
     if (credentials) {
+      console.log(credentials)
       // Attempt to retrieve the user from the data store
       const users = await User.findAll();
+      // console.log(users)
       const user = users.find(data => data.emailAddress === credentials.name);
       // If a user was successfully retrieved from the data store...
       if (user) {
@@ -21,13 +25,16 @@ const authenticateUser = async (req, res, next) => {
         );
         // If the passwords match...
         if (authenticated) {
+          // console.log(authenticated)
           // Store the retrieved user object on the request object.
           req.currentUser = user;
+          // console.log(user)
+          // console.log(req.currentUser);
         } else {
           message = `Authentication failure for username: ${user.emailAddress}`;
         }
       } else {
-          message = `Authentication failure for username: ${user.emailAddress}`;
+          message = `Authentication failure for username: `;
       }
     } else {
         message = 'Auth header not found';
